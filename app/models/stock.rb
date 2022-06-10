3901fed2b2238b02e4f3e09f762f8042ff5e4838
William <wdmcdonough@gmail.com>
2022-06-09 19:44:04 -0500

    IEX API search added

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -3,6 +3,12 @@ class Stock < ApplicationRecord
   def self.new_lookup(ticker_symbol) #self makes it so I don't need instance var
     client = IEX::Api::Client.new(publishable_token: Rails.application.credentials.iex_client[:sandbox_api_key],
                                   endpoint: 'https://sandbox.iexapis.com/v1')
-    client.price(ticker_symbol) #implied return, thanks ruby
+    begin
+      new(ticker: ticker_symbol, 
+          name: client.company(ticker_symbol).company_name, 
+          last_price: client.price(ticker_symbol))
+      rescue => exception
+        return nil
+      end
   end
 end
