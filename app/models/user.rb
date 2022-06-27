02448f9152b02d7009bd46c523279597036bb599
William <wdmcdonough@gmail.com>
2022-06-26 19:40:23 -0500

    finish up friends features, users show page

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,4 +26,33 @@ class User < ApplicationRecord
     return "#{first_name.capitalize} #{last_name.capitalize}" if first_name || last_name
     "Guest"
   end
+
+  def self.search(param)
+    param.strip!
+    to_send_back = (first_name_matches(param) + 
+                    last_name_matches(param) + 
+                    email_matches(param)).uniq
+    return nil unless to_send_back
+    to_send_back
+  end
+
+  def self.first_name_matches(param)
+    matches('first_name', param)
+  end
+
+  def self.last_name_matches(param)
+    matches('last_name', param)
+  end
+
+  def self.email_matches(param)
+    matches('email', param)
+  end
+
+  def self.matches(field_name, param)
+    User.where("#{field_name} like ?", "%#{param}%")
+  end
+
+  def not_friends_with?(id_of_friend)
+    !self.friends.where(id: id_of_friend).exists?
+  end
 end
